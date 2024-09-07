68745ad441d36a2cc6d986c16b8f080bf81b04c8
yiadka <moondrip4869@gmail.com>
2024-09-07 22:38:55 +0900

    特定のページをGETする

--- api/main.go
+++ api/main.go
@@ -37,9 +37,23 @@ func postAlbums(c *gin.Context) {
 	c.IndentedJSON(http.StatusCreated, newAlbum)
 }
 
+func getAlbumById(c *gin.Context) {
+	id := c.Param("id")
+
+	for _, a := range albums {
+		if a.ID == id {
+			c.IndentedJSON(http.StatusOK, a)
+			return
+		}
+	}
+	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "album not found"})
+}
+
 func main() {
 	router := gin.Default()
 	router.GET("/albums", getAlbums)
+	// router.GET("/albums/:id", getAlbumById)
+	router.GET("/albums/:id", getAlbumById)
 	router.POST("/albums", postAlbums)
 
 	router.Run(":8080")
